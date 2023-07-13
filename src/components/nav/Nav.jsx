4b9bec71c86d2ea8e9ca6857c893b3d93ab5b93e
dangol429 <109843656+dangol429@users.noreply.github.com>
2023-07-13 13:01:59 +0530

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './Nav.css'
 import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = (props) => {
   /* Creating a simple navigation bar has 3 options each one linked using routing */
   return (
           <nav>
-                <Link to='/task1'>Task 1</Link>
-                <Link to='/task2'>Task 2</Link>
-                <Link to='/task3'>Task 3</Link>
+                <Link className = {props.task1} to='/task1'>Task 1</Link>
+                <Link className = {props.task1} to='/task2'>Task 2</Link>
+                <Link className = {props.task3} to='/task3'>Task 3</Link>
 
           </nav>
   )
